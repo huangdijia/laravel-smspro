ce4f2ef3c9e5cdc9006e10d04ef7cf701a61d42c
huangdijia <huangdijia@gmail.com>
2018-11-29 14:12:30 +0800

    support account info

--- config/config.php
+++ config/config.php
@@ -1,6 +1,5 @@
 <?php
 return [
-    "api"      => 'https://api3.hksmspro.com/service/smsapi.asmx/SendSMS',
     "username" => env('SMSPRO_USERNAME', ''),
     "password" => env('SMSPRO_PASSWORD', ''),
     "sender"   => env('SMSPRO_SENDER', ''),
